b6e04927c71d7b29261a8b4728249f9a25a418fb
abhixhek411 <147336416+abhixhek411@users.noreply.github.com>
2025-09-09 16:45:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className="relative min-h-screen overflow-y-scroll text-white ">
       <MatrixBackground />
 
-      <div className="absolute inset-0 flex items-center justify-center z-10 p-4">
+      <div className="absolute inset-0 flex items-start justify-center z-10 p-4">
         <div className="bg-black/70 backdrop-blur-md border border-white-800 shadow-lg p-8 rounded-xl w-full max-w-2xl space-y-6 text-center">
           <h1 className="text-3xl font-bold bg-red-950 text-white drop-shadow-md xxx ">
             🔰 Threat Summary
